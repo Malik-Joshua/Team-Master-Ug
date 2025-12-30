eab7d0ce29111bef8680a8935e2c8fac32333bea
Malik Joshua <malikdesa51@gmail.com>
2025-12-30 21:02:25 +0200

    Fix TypeScript error: add useEffect import to TopBar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { Bell, MessageSquare, X, CheckCheck } from 'lucide-react'
 import { cn } from '@/lib/utils'
